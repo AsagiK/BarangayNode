b941e16f53cbc23ba5484d63f3e4cb9c9cbe6888
AsagiK <42568800+AsagiK@users.noreply.github.com>
2020-04-17 14:36:16 +0800

    AJAX Register

--- routes.js
+++ routes.js
@@ -3,5 +3,6 @@ var SessCtrl = require('./controllers/Sessioncontroller');
 var router = express.Router();
 
 router.route('/RegistrationForm').get(SessCtrl.RegistrationForm);
+router.route('/RegisterUser').post(SessCtrl.RegisterUser);
 
 module.exports = router;
